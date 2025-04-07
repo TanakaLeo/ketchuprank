866bb9d9a1872e8b6ce1ad91013ebf797f0e0bdb
Leo-Tanaka <RM556781@fiap.com.br>
2025-04-07 12:05:04 -0300

    added ketchup splash. not blood. probably. aaand a lil' surprise at the end ;)

--- script/main.js
+++ script/main.js
@@ -1,8 +1,17 @@
 document.getElementById('prank-button').addEventListener('click', () => {
+    const button = document.getElementById('prank-button')
     const audio = document.getElementById('ketchupprank');
+    button.style.display = 'none';
     audio.currentTime = 0;
     audio.play();
     explodeKetchup();
+    showKetchupSplash();
+    setTimeout(() => {
+      showGasterBlaster();
+    }, 24000);
+    setTimeout(() => {
+      resetPrank();
+    }, 25000);
   });
   
   function explodeKetchup() {
@@ -30,4 +39,40 @@ document.getElementById('prank-button').addEventListener('click', () => {
       setTimeout(() => drop.remove(), 1100);
     }
   }
+  function showKetchupSplash() {
+    const splash = document.createElement('img')
+    splash.src = 'assets/img/ketchup.png';
+    splash.id = 'splash-ketchup';
+    splash.style.position = 'fixed';
+    splash.style.left = '50%';
+    splash.style.top = '50%';
+    splash.style.transform = 'translate(-50%, -50%)';
+    splash.style.width = '700px';
+    splash.style.pointerEvents = 'none';
+    splash.style.zIndex = '20';
   
+    document.body.appendChild(splash);
+  }
+  function showGasterBlaster() {
+    const gaster = document.createElement('img');
+    gaster.src = 'assets/img/gasterblaster.png';
+    gaster.id = 'gasterblaster';
+    gaster.style.position = 'fixed';
+    gaster.style.left = '50%';
+    gaster.style.top = '50%';
+    gaster.style.transform = 'translate(-50%, -50%)';
+    gaster.style.width = '100vw';
+    gaster.style.height = '100vh';
+    gaster.style.objectFit = 'cover';
+    gaster.style.zIndex = '999';
+    gaster.style.pointerEvents = 'none';
+    gaster.style.backgroundColor = 'black';
+  
+    document.body.appendChild(gaster);
+  }
+  
+  function resetPrank() {
+    document.querySelectorAll('.ketchup, #splash-ketchup, #gasterblaster').forEach(el => el.remove());
+    const button = document.getElementById('prank-button');
+    button.style.display = 'block';
+  }
